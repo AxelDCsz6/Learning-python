0df17a5b7bad5cefcc8771087676e04cf8d79e05
AxelDCsz6 <axeldc585@gmail.com>
2026-01-04 13:31:44 -0600

    practica dia 4, control de flujos

--- intro/control_flows.py
+++ intro/control_flows.py
@@ -3,6 +3,15 @@ import sys
 TRUTHY = {"true","t","1","yes","y"}
 FALSY = {"false","f","0","no","n"}
 
+#functions definition
+def fib(n):
+    """Todo lo puesto aqui sera tomado para python como un string de documentacion de la propia funcion"""
+    a,b = 0,1
+    while a < n:
+        print(a, end=", ")
+        a, b = b, a+b
+    print()
+
 #if statement and try-except
 try:
     if len(sys.argv)<2:
@@ -16,7 +25,49 @@ try:
         print("This is a false statement")
     else:
         print("valor no valido")
+
+    if len(sys.argv) > 2:
+        fib(int(sys.argv[2]))
 except SystemExit:
     print("No argumentos entregados")
+
+#for statements
+print("Valores verdaderos:")
+for x in TRUTHY:
+    print(x, end=", ")
+
+for x in range(10):
+    print(x, end=", ")
+
+print()
+users = {'Hans': 'active', 'Éléonore': 'inactive', 'Jose': 'active'}
+
+for user, status in users.items():
+    if status == 'active':
+        print(user)
+
+step, start, end = 2, -10, 10
+
+print(list(range(start, end, step)))
+
+for x in enumerate(users):
+    print(x)
+
+for user, status in users.items():
+    if status == 'active':
+       continue 
+    print(user)
+
+#match statement
+numero = 1
+match numero:
+    case 0:
+        print("esta linea no se ejecutara")
+    case 1:
+        print("esta linea si se ejecutara")
+    case 2:
+        print("esta linea no se ejecutara")
+    case 3:
+        print("esta linea no se ejecutara")
 
 
